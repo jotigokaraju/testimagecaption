73138b33df8317c88d34b60d42d7dd08f805b470
jotigokaraju <148422591+jotigokaraju@users.noreply.github.com>
2024-04-13 11:50:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,13 @@
-from transformers import GPT2TokenizerFast, ViTImageProcessor, VisionEncoderDecoderModel
-from torch.utils.data import Dataset
-from torchtext.data import get_tokenizer
-import requests
-import torch
-import numpy as np
-from PIL import Image
-import pickle
-import matplotlib.pyplot as plt
-import os
-from tqdm import tqdm
 import streamlit as st
-import warnings
-warnings.filterwarnings('ignore')
+from transformers import pipeline
+from PIL import Image
 
+caption=pipeline('image-to-text', model="ydshieh/vit-gpt2-coco-en")
+upload = st.file_uploader("Upload an Image", type=["png", "jpg", "jpeg"])
+if upload is not None:
+    image = Image.open(upload)
+    st.image(image)
 
-model_raw = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-image_processor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-tokenizer = GPT2TokenizerFast.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-
-def show_n_generate(url, greedy = True, model = model_raw):
-    image = Image.open(requests.get(url, stream =True).raw)
-    pixel_values   = image_processor(image, return_tensors ="pt").pixel_values
-
-    if greedy:
-        generated_ids  = model.generate(pixel_values, max_new_tokens = 30)
-    else:
-        generated_ids  = model.generate(
-            pixel_values,
-            do_sample=True,
-            max_new_tokens = 30,
-            top_k=5)
-    generated_text = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-    return generated_text
-
-image = "http://images.cocodataset.org/val2017/000000039769.jpg"
-if st.button("run"):
-    answer = show_n_generate(image, greedy = False)
-    st.write(answer)
+is st.button("Caption"):
+captions = caption(image)
+st.write(captions[0]['generated_text])
