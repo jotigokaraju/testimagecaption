027f7b8e07280f064529cce5dffb3592a013d2da
jotigokaraju <148422591+jotigokaraju@users.noreply.github.com>
2024-04-13 12:01:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,27 @@ import streamlit as st
 from transformers import pipeline
 from PIL import Image
 
-caption=pipeline('image-to-text', model="ydshieh/vit-gpt2-coco-en")
-upload = st.file_uploader("Upload an Image", type=["png", "jpg", "jpeg"])
-if upload is not None:
-    image = Image.open(upload)
-    st.image(image)
+# Load the pipeline outside Streamlit script
+caption = None
 
-if st.button("Caption"):
-    captions = caption(image)
-    st.write(captions[0]['generated_text'])
+@st.cache(allow_output_mutation=True)
+def load_model():
+    return pipeline('image-to-text', model="ydshieh/vit-gpt2-coco-en")
+
+# Streamlit script
+def main():
+    global caption
+    if caption is None:
+        caption = load_model()
+
+    upload = st.file_uploader("Upload an Image", type=["png", "jpg", "jpeg"])
+    if upload is not None:
+        image = Image.open(upload)
+        st.image(image)
+
+    if st.button("Caption") and image is not None:
+        captions = caption(image)
+        st.write(captions[0]['generated_text'])
+
+if __name__ == "__main__":
+    main()
