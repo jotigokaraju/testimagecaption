192e8791e915d864751e92919de2ea1c90b981d9
jotigokaraju <148422591+jotigokaraju@users.noreply.github.com>
2024-04-16 19:56:50 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,45 @@
 import streamlit as st
 from transformers import pipeline
 from PIL import Image
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 
 # Load the pipeline outside Streamlit script
 caption = None
 
+@st.cache_resource
+def load_processor():
+    return TrOCRProcessor.from_pretrained('microsoft/trocr-base-str')
+
 @st.cache(allow_output_mutation=True)
 def load_model():
-    return pipeline('image-to-text', model="ydshieh/vit-gpt2-coco-en")
+    VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-str')
+
+pixel_values = processor(images=image, return_tensors="pt").pixel_values
 
 # Streamlit script
 def main():
     global caption
     if caption is None:
-        caption = load_model()
+        model = load_model()
+        processor = load_processor()
 
-    upload = st.file_uploader("Upload an Image", type=["png", "jpg", "jpeg"])
-    if upload is not None:
-        image = Image.open(upload)
-        st.image(image)
+    photo = st.camera_input("Take a picture")
+    if photo is not None:
+        image = Image.open(photo).convert("RGB")
+        st.image(Image.open(photo))
 
     if st.button("Caption") and image is not None:
-        captions = caption(image)
-        st.write(captions[0]['generated_text'])
+        generated_ids = model.generate(pixel_values)
+        generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        st.write(generated_text)
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
